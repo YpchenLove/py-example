fd957fda3cb16d9392d6eff2f84feb8d03ab849e
Ypchen <ypchng@qq.com>
2019-08-22 16:03:51 +0800

    feat: add string

---
+++ 1-base.py
@@ -0,0 +1,21 @@
+import keyword
+
+print(keyword.kwlist)
+
+# 注释
+
+'''
+这里也是注释
+'''
+
+"""
+这也是注释
+"""
+print("Hello, World!")
+
+# 缩进
+
+if True:
+    print('True')
+else:
+    print('False')
---
+++ 2-number.py
@@ -0,0 +1,5 @@
+
+print(type(1 + 2j))
+print(type(True))
+print(type(5))
+print(type(1.5))
